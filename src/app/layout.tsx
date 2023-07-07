0f0443fd3aa98341d6933d151afc4d626eb7a923
Faiz Alkautsar <alkafaiz99@gmail.com>
2023-07-08 01:55:24 +0800

    change meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'No Monkey Work',
+  title: 'Happy task hub',
   description: 'Helping you to automate your life',
 }
 
